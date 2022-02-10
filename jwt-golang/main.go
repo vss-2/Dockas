9ac8880f45519c2d6c76a210b92d3a35fc4a0574
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2022-02-10 11:24:57 -0300

    Unused variables fix

--- jwt-golang/main.go
+++ jwt-golang/main.go
@@ -8,6 +8,8 @@ import (
 	dotenv "github.com/joho/godotenv"
 )
 
+var jwtsecret string
+
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Homepage")
 	fmt.Println("User accessed Homepage")
@@ -17,16 +19,16 @@ func isValid(w http.ResponseWriter, r *http.Request){
 	if r.Header["Token"] != nil {
 		// Esse interface é um objeto (JSON-like) pode ser usado para melhor compreensão do erro
 		token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error){
-			
+
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				// token = nil, erro = erro
-				return nil, fmt.Errorf("Error while trying signing method")
+				return nil, fmt.Errorf("Error while trying signing method", ok)
 			}
 
 			// token = secret, erro = nil
 			return jwtsecret, nil
 		})
-
+		_, _ = token, err
 	} else {
 		fmt.Fprintf(w, "Access not authorized")
 	}
@@ -43,7 +45,6 @@ func handleRequests(){
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-var jwtsecret string
 func main(){
 	envs, err := dotenv.Read(".env")
 	if err != nil {
@@ -51,5 +52,6 @@ func main(){
 	}
 
 	jwtsecret := envs["JWTSECRET"]
+	fmt.Println(jwtsecret)
 	handleRequests()
 }
