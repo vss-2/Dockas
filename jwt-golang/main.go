1604f622a836b1777270c7b5c3b6010398d4045f
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2023-06-23 01:09:30 -0300

    Revert "Merging atual branch with main"
    This reverts commit 8d2c73fe37cb7bcd21054af05e9b79ebd300a4bc, reversing
    changes made to 61ce0081dcec2b679ed054fe7581a740efb9df40.

--- jwt-golang/main.go
+++ jwt-golang/main.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	jwt "github.com/golang-jwt/jwt"
 	dotenv "github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	_ "strings"
@@ -52,6 +53,92 @@ func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Println("User accessed Homepage")
 }
 
+// https://www.honeybadger.io/blog/go-web-services/
+// https://golangbyexample.com/200-http-status-response-golang/
+func userRegister(w http.ResponseWriter, r *http.Request){
+	if (r.Header["Name_first"] != nil && 
+		r.Header["Name_last"] != nil &&
+		r.Header["Email"] != nil &&
+		r.Header["Password"] != nil){
+			http.Error(w, "Missing one or more parameters: first name, last name, email or password", http.StatusBadRequest)
+			return
+	} else {
+		cursor, err := collection.Find(ctx, bson.M{})
+		if(err != nil){
+			log.Fatal(err)
+		}
+
+		// Query if there's already a user with the same email registered
+		var user []bson.M
+		if err = cursor.All(ctx, bson.M{"Email": r.Header["Email"]}, &user); err != nil {
+			log.Fatal(err)
+		}
+		// If there isn't insert one in
+		if(len(user) == 0){
+			token, err := jwt.createToken(envs["JWTSECRET"]); err != nil {
+				log.Fatal(err)
+			}
+			if res, err := cursor.InsertOne(ctx.Background(), bson.M{
+				"Name_first": r.Header["Name_firl"], 
+				"Name_last": r.Header["Name_last"], 
+				"Email": r.Header["Email"],
+				"Password": r.Header["Password"],
+				"Token": token,
+				"expiresAfterSeconds": 60*60*12
+			});
+			err != nil {
+				log.Fatal(err)
+			} else {
+				// Return request correctly
+				resp := make(map[string]string)
+				resp["message"] = "Registered with success"
+				resp["Token"] = token
+				if jsonResp, err := json.Marshal(resp); err != nil {
+					log.Fatal(err)
+				}
+				w.WriterHeader(http.StatusOK)
+				w.Write(jsonResp)
+				return
+			}
+		} else {
+			http.Error(w, "User already registered in database, did you forget you password?", http.Conclict)
+		}
+	}
+}
+
+func userLogin(w http.ResponseWriter, r *http.Request){
+	if (r.Header["Name_first"] != nil && 
+		r.Header["Name_last"] != nil &&
+		r.Header["Password"] != nil){
+			r.Header["Email"] != nil &&
+			// fmt.Fprintf("Missing email or password")
+			http.Error(w, "Missing email or password", http.StatusBadRequest)
+			return
+	} else {
+		var user []bson.M
+		if err = cursor.All(ctx, bson.M{"Email": r.Header["Email"]}, &user); err != nil {
+			log.Fatal(err)
+		}
+		if(len(user) == 1){
+			token, err := jwt.createToken(envs["JWTSECRET"]); err != nil {
+				log.Fatal(err)
+			}
+			resp := make(map[string]string)
+			resp["message"] = "Login with success"
+			resp["Token"] = token
+			if jsonResp, err := json.Marshal(resp); err != nil {
+				log.Fatal(err)
+			}
+			w.WriterHeader(http.StatusOK)
+			w.Write(jsonResp)
+			return
+			}
+		} else {
+			http.Error(w, "Incorrect email or password", http.Forbidden)
+		}
+	}
+}
+
 func isValid(w http.ResponseWriter, r *http.Request){
 	if r.Header["Token"] != nil {
 		// Esse interface é um objeto (JSON-like) pode ser usado para melhor compreensão do erro
@@ -71,7 +158,8 @@ func isValid(w http.ResponseWriter, r *http.Request){
 
 		_, _ = token, err
 	} else {
-		fmt.Fprintf(w, "Access not authorized")
+		// fmt.Fprintf(w, "Access unauthorized")
+		http.Error(w, "Access unauthorized, try to login first", http.Unauthorized)
 	}
 }
 
@@ -84,6 +172,7 @@ func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handle
 func handleRequests(envs map[string]string){
 	http.HandleFunc("/", isValid)
 	http.HandleFunc("/favicon.ico", favicon)
+	http.HandleFunc("/register", userRegister)
 	log.Fatal(http.ListenAndServe(string(":")+string(envs["APIPORT"]), nil))
 }
 
